ec59c6f6cfe93fe6fbc704e955f4416bad7c2b5a
Vasyl Piellia <pellia@ukr.net>
2017-04-07 17:38:39 +0300

    Create Articles app

--- dance/urls.py
+++ dance/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from articles import views as articles_views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^articles/$', articles_views.article_list_show)
 ]
