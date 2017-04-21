a2df1b9a259152c70b887d98a99b447f049fa216
Vasyl Piellia <pellia@ukr.net>
2017-04-21 17:57:16 +0300

    Divide events into months
    (cherry picked from commit 1b59487)

--- entities/models.py
+++ entities/models.py
@@ -11,6 +11,9 @@ class UserProfile(models.Model):
 
     role = models.CharField(max_length=50, blank=True, default=None, null=True)
 
+    def name(self):
+        return self.user.name
+
     def __str__(self):
         return "%s's profile" % self.user
 
@@ -48,7 +51,7 @@ class DanceType(models.Model):
     # parent_dance_type =
     # child_dance_type =
 
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, )
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -63,7 +66,7 @@ class EventType(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
 
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, )
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -83,9 +86,11 @@ class Location(models.Model):
     dance_types = models.ManyToManyField(DanceType, blank=True)
 
     def get_dance_types(self):
-        return "\n".join([p.dance_types for p in self.dance_types.all()])
+        if self.dance_types.all():
+            return "\n".join([p.title for p in self.dance_types.all()])
+        return ''
 
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, )
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -112,19 +117,29 @@ class Event(models.Model):
         (POSTPONED, 'Перенесено'),
         (HELD, 'Проведено'),
     )
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PLANNED, blank=True, )
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, default=PLANNED, blank=True)
 
-    start_date = models.DateField(default=date.today, blank=True, )
-    end_date = models.DateField(default=date.today, blank=True, )
+    start_date = models.DateField(default=date.today, blank=True, null=True)
+    end_date = models.DateField(default=date.today, blank=True, null=True)
 
     def date(self):
-        return '{0} - {1}'.format(self.start_date.strftime('%d.%m'), self.end_date.strftime('%d.%m'))
+        if self.start_date and self.end_date:
+            return '{0} - {1}'.format(self.start_date.strftime('%d.%m'), self.end_date.strftime('%d.%m'))
+        if self.start_date:
+            return 'c {0}'.format(self.start_date.strftime('%d.%m'),)
+        if self.end_date:
+            return 'по {0}'.format(self.end_date.strftime('%d.%m'), )
+        return 'Неизвестно'
 
     def get_start_date_day_of_week(self):
-        return ''.join(self.start_date.strftime("%A"))
+        if self.start_date:
+            return ''.join(self.start_date.strftime("%A"))
+        return ''
 
     def get_end_date_day_of_week(self):
-        return ''.join(self.end_date.strftime("%A"))
+        if self.end_date:
+            return ''.join(self.end_date.strftime("%A"))
+        return ''
 
     event_types = models.ManyToManyField(EventType, blank=True)
     dance_types = models.ManyToManyField(DanceType, blank=True)
@@ -132,18 +147,26 @@ class Event(models.Model):
     links = models.ManyToManyField(Link, blank=True)
 
     def get_event_types(self):
-        return "\n".join([p.title for p in self.event_types.all()])
+        if self.event_types.all():
+            return "\n".join([p.title for p in self.event_types.all()])
+        return ''
 
     def get_dance_types(self):
-        return "\n".join([p.title for p in self.dance_types.all()])
+        if self.dance_types.all():
+            return "\n".join([p.title for p in self.dance_types.all()])
+        return ''
 
     def get_locations(self):
-        return "\n".join([p.title for p in self.locations.all()])
+        if self.locations.all():
+            return "\n".join([p.title for p in self.locations.all()])
+        return ''
 
     def get_links(self):
-        return "\n".join([p.link for p in self.links.all()])
+        if self.links.all():
+            return "\n".join([p.link for p in self.links.all()])
+        return ''
 
-    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE, )
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 
@@ -152,3 +175,178 @@ class Event(models.Model):
 
     class Meta:
         ordering = ('updated',)
+
+
+class Instructor(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    image = models.ImageField(blank=True)
+
+    dance_types = models.ManyToManyField(DanceType, blank=True)
+    events = models.ManyToManyField(Event, blank=True)
+    links = models.ManyToManyField(Link, blank=True)
+
+    def get_dance_types(self):
+        if self.dance_types.all():
+            return "\n".join([p.title for p in self.dance_types.all()])
+        return ''
+
+    def get_events(self):
+        if self.events.all():
+            return "\n".join([p.title for p in self.events.all()])
+        return ''
+
+    def get_links(self):
+        if self.links.all():
+            return "\n".join([p.link for p in self.links.all()])
+        return ''
+
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return '%s' % self.name
+
+    class Meta:
+        ordering = ('name',)
+
+
+class DanceStudio(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    logo = models.ImageField(blank=True)
+
+    dance_types = models.ManyToManyField(DanceType, blank=True)
+    instructors = models.ManyToManyField(Instructor, blank=True)
+    locations = models.ManyToManyField(Location, blank=True)
+    links = models.ManyToManyField(Link, blank=True)
+
+    def get_dance_types(self):
+        if self.dance_types.all():
+            return "\n".join([p.title for p in self.dance_types.all()])
+        return ''
+
+    def get_instructors(self):
+        if self.instructors.all():
+            return "\n".join([p.title for p in self.instructors.all()])
+        return ''
+
+    def get_locations(self):
+        if self.locations.all():
+            return "\n".join([p.title for p in self.locations.all()])
+        return ''
+
+    def get_links(self):
+        if self.links.all():
+            return "\n".join([p.link for p in self.links.all()])
+        return ''
+
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        return '%s' % self.title
+
+    class Meta:
+        ordering = ('title',)
+
+
+class WeekDay(models.Model):
+    MONDAY = 'MON'
+    TUESDAY = 'TUE'
+    WEDNESDAY = 'WED'
+    THURSDAY = 'THU'
+    FRIDAY = 'FRI'
+    SATURDAY = 'SAT'
+    SUNDAY = 'SUN'
+
+    DAY_CHOICES = (
+        (MONDAY, 'Понедельник'),
+        (TUESDAY, 'Вторник'),
+        (WEDNESDAY, 'Среда'),
+        (THURSDAY, 'Четверг'),
+        (FRIDAY, 'Пятница'),
+        (SATURDAY, 'Суббота'),
+        (SUNDAY, 'Воскресенье'),
+    )
+
+    day = models.CharField(max_length=3, choices=DAY_CHOICES, blank=True)
+
+    def __str__(self):
+        return '%s' % self.day
+
+
+class DanceClass(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+
+    first_lesson_free = models.BooleanField(default=False, blank=True)
+    free_lesson_date = models.DateField(default=date.today, null=True, blank=True)
+    every_first_lesson_free = models.BooleanField(default=False, blank=True)
+
+    start_date = models.DateField(default=date.today, null=True, blank=True)
+    end_date = models.DateField(default=date.today, null=True, blank=True)
+    schedule_week_days = models.ManyToManyField(WeekDay, blank=True)
+
+    def get_schedule_week_days(self):
+        if self.schedule_week_days.all():
+            return "\n".join([p.day for p in self.schedule_week_days.all()])
+        return ''
+
+    dance_studio = models.ForeignKey(DanceStudio, on_delete=models.CASCADE, null=True, blank=True)
+    dance_types = models.ManyToManyField(DanceType, blank=True)
+    instructors = models.ManyToManyField(Instructor, blank=True)
+    links = models.ManyToManyField(Link, blank=True)
+
+    def get_dance_types(self):
+        if self.dance_types.all():
+            return "\n".join([p.title for p in self.dance_types.all()])
+        return ""
+
+    def get_instructors(self):
+        if self.instructors.all():
+            return "\n".join([p.title for p in self.instructors.all()])
+        return ''
+
+    def get_links(self):
+        if self.links.all():
+            return "\n".join([p.link for p in self.links.all()])
+        return ''
+
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        if self.dance_studio:
+            return '%s in %s' % (self.title, self.dance_studio.title)
+        return '%s' % (self.title,)
+
+    class Meta:
+        ordering = ('title',)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+
+    image = models.ImageField(blank=True)
+
+    is_linked_article = models.BooleanField(default=False)
+    article_link = models.URLField(blank=True)
+
+    author = models.ForeignKey('UserProfile', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+    def __str__(self):
+        if self.author:
+            return '%s by %s' % (self.title, self.author.user.name)
+        return '%s' % (self.title,)
+
+    class Meta:
+        ordering = ('created',)
--- event_scheme/views.py
+++ event_scheme/views.py
@@ -1,13 +1,65 @@
+import datetime
+
+from dateutil.rrule import rrule, MONTHLY
 from django.shortcuts import render
 
 from entities.models import Event
 
+MONTH_INT_TO_STR = {
+    '1': 'Январь',
+    '2': 'Февраль',
+    '3': 'Март',
+    '4': 'Апрель',
+    '5': 'Май',
+    '6': 'Июнь',
+    '7': 'Июль',
+    '8': 'Август',
+    '9': 'Сентябрь',
+    '10': 'Октябрь',
+    '11': 'Ноябрь',
+    '12': 'Декабрь'
+}
+
 
 def event_scheme_show(request):
-    title = 'EventScheme'
-    events = Event.objects.all()
+    title = 'Расписание мероприятий'
+
+    # Dividing events into months
+    month_year_set_start_date = \
+        set((event_date.month, event_date.year,) for event_date in Event.objects.dates('start_date', 'month'))
+    month_year_set_end_date = \
+        set((event_date.month, event_date.year,) for event_date in Event.objects.dates('end_date', 'month'))
+    month_year_list_start_end_date = sorted(month_year_set_start_date | month_year_set_end_date)
+
+    events_months = []
+    if month_year_list_start_end_date:
+        first_date = datetime.date(month_year_list_start_end_date[0][1], month_year_list_start_end_date[0][0], 1)
+        last_date = datetime.date(month_year_list_start_end_date[-1][1], month_year_list_start_end_date[-1][0], 1)
+        dates = [(dt.month, dt.year) for dt in rrule(MONTHLY, dtstart=first_date, until=last_date)]
+
+        for month, year in dates:
+            events = [event for event in Event.objects.filter(
+                start_date__gte=datetime.datetime(year, month, 1), end_date__lt=datetime.datetime(year, month+1, 1)
+                ) | Event.objects.filter(
+                start_date__lt=datetime.datetime(year, month+1, 1), end_date__gte=datetime.datetime(year, month, 1)
+                ) | Event.objects.filter(
+                start_date__isnull=True,
+                end_date__gte=datetime.datetime(year, month, 1),
+                end_date__lt=datetime.datetime(year, month+1, 1)
+                ) | Event.objects.filter(
+                end_date__isnull=True,
+                start_date__gte=datetime.datetime(year, month, 1),
+                start_date__lt=datetime.datetime(year, month+1, 1)
+                )
+                      ]
+
+            if events:
+                events_months.append({'month': '%s %s' % (MONTH_INT_TO_STR[str(month)], year),
+                                      'events': events
+                                      })
+
     context = {
         'title': title,
-        'events': events
+        'events_months': events_months
     }
     return render(request, 'event_scheme.html', context)
