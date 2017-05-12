bba84f14bd0f4d1a900f6dfbd6b625c2dc19c639
Vasyl Piellia <pellia@ukr.net>
2017-05-12 15:24:27 +0300

    Remove unnecessary code

--- dance_styles/views.py
+++ dance_styles/views.py
@@ -5,9 +5,7 @@ from entities.models import DanceStyle
 
 def dance_styles_show(request):
     title = 'Танцевальные стили'
-    styles_directions = [{'dance_direction': 'Бальные танцы', 'dance_styles': ['1']},
-                         {'dance_direction': 'Латина', 'dance_styles': ['2']}
-                         ]
+
     dance_styles = DanceStyle.objects.all()
     directions = {'BAL', 'LAT'}
     dance_direction_show = {
@@ -27,32 +25,3 @@ def dance_styles_show(request):
         'styles_directions': styles_directions
     }
     return render(request, 'dance_styles/dance_styles.html', context)
-
-
-
-    instances_months = []
-    if month_year_list_start_end_date:
-        first_date = datetime.date(month_year_list_start_end_date[0][1], month_year_list_start_end_date[0][0], 1)
-        last_date = datetime.date(month_year_list_start_end_date[-1][1], month_year_list_start_end_date[-1][0], 1)
-        dates = [(dt.month, dt.year) for dt in rrule(MONTHLY, dtstart=first_date, until=last_date)]
-
-        for month, year in dates:
-            instances = [instance for instance in entity.objects.filter(
-                start_date__gte=datetime.datetime(year, month, 1), end_date__lt=datetime.datetime(year, month + 1, 1)
-            ) | entity.objects.filter(
-                start_date__lt=datetime.datetime(year, month + 1, 1), end_date__gte=datetime.datetime(year, month, 1)
-            ) | entity.objects.filter(
-                start_date__isnull=True,
-                end_date__gte=datetime.datetime(year, month, 1),
-                end_date__lt=datetime.datetime(year, month + 1, 1)
-            ) | entity.objects.filter(
-                end_date__isnull=True,
-                start_date__gte=datetime.datetime(year, month, 1),
-                start_date__lt=datetime.datetime(year, month + 1, 1)
-            )
-                      ]
-
-            if instances:
-                instances_months.append({'month': '%s %s' % (MONTH_INT_TO_STR[str(month)], year),
-                                        'instances': instances
-                                        })
