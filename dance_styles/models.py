93f6360fdf93289c1d466c54ae1b54f2cb70c149
Vasyl Piellia <pellia@ukr.net>
2017-05-26 19:04:21 +0300

    minor change

--- dance_styles/models.py
+++ dance_styles/models.py
@@ -1,57 +1,6 @@
 from django.db import models
 
 
-class CountType(models.Model):
-    SOLO = 'SOL'
-    PARTNER = 'PRN'
-    GROUP = 'GRP'
-
-    COUNT_TYPE_CHOICES = (
-        (SOLO, 'Одиночный'),
-        (PARTNER, 'Парный'),
-        (GROUP, 'Групповой')
-    )
-
-    count_type = models.CharField(max_length=3, choices=COUNT_TYPE_CHOICES, blank=True)
-
-    def __str__(self):
-        return '%s' % self.count_type
-
-
-class BetweenPartnersDistance(models.Model):
-    CLOSE = 'CLS'
-    AREVAGE = 'AVG'
-    DISTANT = 'DIS'
-
-    DISTANCE_CHOICES = (
-        (CLOSE, 'Близкое'),
-        (AREVAGE, 'Среднее'),
-        (DISTANT, 'Далекое')
-    )
-
-    distance = models.CharField(max_length=3, choices=DISTANCE_CHOICES, blank=True)
-
-    def __str__(self):
-        return '%s' % self.distance
-
-
-class AveragePrice(models.Model):
-    LOW = 'LOW'
-    AVERAGE = 'AVG'
-    HIGH = 'HIG'
-
-    PRICE_CHOICES = (
-        (LOW, 'Низкая'),
-        (AVERAGE, 'Средняя'),
-        (HIGH, 'Высокая')
-    )
-
-    price = models.CharField(max_length=3, choices=PRICE_CHOICES, blank=True)
-
-    def __str__(self):
-        return '%s' % self.price
-
-
 class DanceStyleInSectionCountType(models.Model):
     SOLO = 'SOL'
     PARTNER = 'PRN'
@@ -75,9 +24,9 @@ class DanceStyleInSectionBetweenPartnersDistance(models.Model):
     DISTANT = 'DIS'
 
     DISTANCE_CHOICES = (
-        (CLOSE, 'Близкое'),
-        (AREVAGE, 'Среднее'),
-        (DISTANT, 'Далекое')
+        (CLOSE, 'Близкая'),
+        (AREVAGE, 'Средняя'),
+        (DISTANT, 'Далекая')
     )
 
     distance = models.CharField(max_length=3, choices=DISTANCE_CHOICES, blank=True)
@@ -92,9 +41,9 @@ class DanceStyleInSectionAveragePrice(models.Model):
     HIGH = 'HIG'
 
     PRICE_CHOICES = (
-        (LOW, 'Низкая'),
-        (AVERAGE, 'Средняя'),
-        (HIGH, 'Высокая')
+        (LOW, 'Низкий'),
+        (AVERAGE, 'Средний'),
+        (HIGH, 'Высокий')
     )
 
     price = models.CharField(max_length=3, choices=PRICE_CHOICES, blank=True)
